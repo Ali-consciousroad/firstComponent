325a59989dc1b2b123986a74f4c06a083cb1593b
ali-consciousroad <ali_dindar@live.be>
2024-07-22 14:39:03 +0200

    Complete code by adding the Heading component inside the App.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 function App() { 
+  const Heading = () => <h1>This is an h1 heading</h1>;
+  
   return ( 
     <div className="App"> 
       This is the starting code for "Your first component" ungraded lab 
